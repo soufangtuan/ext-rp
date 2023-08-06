c15fe8e56560db617d2b60d5b829bad3894a0410
soufangtuan <111952181+soufangtuan@users.noreply.github.com>
2023-08-06 11:36:54 +0800

    Update install_rd.sh

--- redpill-dtb-static/releases/install_rd.sh
+++ redpill-dtb-static/releases/install_rd.sh
@@ -7,5 +7,5 @@
 # copy file
 # cp -vf model_${PLATFORM_ID%%_*}.dtb /etc.defaults/model.dtb
 # cp -vf model_${PLATFORM_ID%%_*}.dtb /var/run/model.dtb
-cp -vf model_dva1622-k4.dtb /etc.defaults/model.dtb
-cp -vf model_dva1622-k4.dtb /var/run/model.dtb
+cp -vf model_dva1622-e.dtb /etc.defaults/model.dtb
+cp -vf model_dva1622-e.dtb /var/run/model.dtb
